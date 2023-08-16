257a33f76ada70794af6b311c0a1aa86f3b6e46a
bgdntrgbn <asda04670@gmail.com>
2023-08-16 12:35:13 +0200

    init commit

---
+++ Homework_1/script.js
@@ -0,0 +1,26 @@
+let s1 = 'hello'
+let s2 = 'owu'
+let s3 = 'com'
+let s4 = 'ua'
+let n1 = 1
+let n2 = 10
+let n3 = -999
+let n4 = 123
+let n5 = 3.14
+let n6 = 2.7
+let n7 = 16
+let b1 = true
+let b2 = false
+console.log(s1, s2, s3, s4, n1, n2, n3, n4, n5, n6, n7, b1, b2)
+
+let name = 'Bohdan'
+let middleName = "None"
+let surname = 'Trehubenko'
+let result = `${name} ${middleName} ${surname}`
+console.log(result)
+
+let a = 100
+let b = '100'
+let c = true
+console.log(typeof a, typeof b, typeof c)
+
