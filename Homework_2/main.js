773f838452566d0e25a0fd1f3a81039c7199c54d
bgdntrgbn <asda04670@gmail.com>
2023-08-20 18:57:45 +0200

    init commit

---
+++ Homework_2/main.js
@@ -0,0 +1,250 @@
+// Task 1
+// let arr = [
+//     '20',
+//     20,
+//     'asdasd',
+//     '11111',
+//     true,
+//     false,
+//     '',
+//     1233,
+//     null,
+//     {
+//         a:'alex'
+//     },
+// ];
+// for(let i= 0; i<arr.length; i++)
+// {
+//     console.log(arr[i])
+// }
+
+//
+// let a = {
+//     tittle: 'HarryPotter',
+//     pageCounts: 500,
+//     genre: 'Fantasy',
+// }
+// let b = {
+//     tittle: 'PiterPen',
+//     pageCounts: 252,
+//     genre: 'Fantasy',
+// }
+// let c = {
+//     tittle: 'FindingNemo',
+//     pageCounts: 300,
+//     genre: 'ScienceFiction',
+//
+// }
+// let h = {
+//     tittle: 'FindingNemo',
+//     pageCounts: 300,
+//     genre: 'ScienceFiction',
+//     authors: {
+//         name: 'Joseph',
+//         age: 35
+//     }
+//
+// }
+// let o = {
+//     tittle: 'FindingNemo',
+//     pageCounts: 300,
+//     genre: 'ScienceFiction',
+//
+// }
+// let j = {
+//     tittle: 'FindingNemo',
+//     pageCounts: 300,
+//     genre: 'ScienceFiction',
+//
+// }
+//
+// let users = [
+//     {
+//         name: 'Bohdan',
+//         username: 'asdasdadsd',
+//         password: '11wewqedw34r!',
+//     },
+//     {
+//         name: 'Sasha',
+//         username: 'shasik',
+//         password: 'vodochka33!',
+//     },
+//     {
+//         name: 'none',
+//         username: 'anonimus',
+//         password: 'sixfivethree!',
+//     },
+//     {
+//         name: 'Luka',
+//         username: 'Luke22',
+//         password: 'mimemamomu!',
+//     },
+//     {
+//         name: 'Horn',
+//         username: '12321',
+//         password: 'mamima!',
+//     },
+//     {
+//         name: 'lexa',
+//         username: 'limk',
+//         password: 'lokuws!',
+//     },
+//     {
+//         name: 'Petya',
+//         username: 'Petro',
+//         password: 'petka11!',
+//     },
+//     {
+//         name: 'Ivan',
+//         username: 'Ivanish',
+//         password: 'Vanka12344erft!',
+//     },
+// {
+//         name: 'Max',
+//         username: 'maxik1234',
+//         password: 'maksik4433!',
+//     },
+// {
+//         name: 'Oleksii',
+//         username: 'Oleksa',
+//         password: 'lexa213r4t5!',
+//     },
+//
+// ]
+// for (let i = 0; i<users.length; i++)
+// {
+//     console.log(users[i].username,  users[i].password)
+// }
+//
+//
+// let x = -3
+// if (x != 0) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// let time = 27
+// if (time <= 14)
+// {
+//     console.log('I')
+// }
+// else if (time > 14 && time < 29)
+// {
+//     console.log('II')
+// }
+// else if (time > 28 && time < 43)
+// {
+//     console.log('III')
+// }
+// else if (time > 42 && time < 60)
+// {
+//     console.log('IV')
+// }
+// else
+// {
+//     console.log('?????')
+// }
+//
+// let day = 1
+// if (day < 11 )
+// {
+//     console.log('I')
+// }
+//
+// else if (day > 10 && day < 21)
+// {
+//     console.log('II')
+// }
+//
+// else if (day > 20 && day < 32)
+// {
+//     console.log('III')
+// }
+// else
+// {
+//     console.log('?????')
+// }
+//
+// let number = prompt('Pick a number of a week day');
+//
+// switch (number)
+// {
+//     case '1':
+//         console.log('Sunday')
+//         break;
+//     case '2':
+//         console.log('Monday')
+//         break;
+//     case '3':
+//         console.log('Tuesday')
+//         break;
+//     case '4':
+//         console.log('Wednesday')
+//         break;
+//     case '5':
+//         console.log('Thursday')
+//         break;
+//     case '6':
+//         console.log('Friday')
+//         break;
+//     case '7':
+//         console.log('Saturday')
+//         break;
+//
+//     default:
+//         console.log('??????')
+// }
+//
+// let n1 = +prompt('Write first number')
+// let n2 = +prompt('Write second number')
+//
+// if (n1 > n2)
+// {
+//     console.log(n1)
+// }
+// else if (n1 < n2)
+// {
+//     console.log(n2)
+// }
+// else if (n1 === n2)
+// {
+//     console.log('Your numbers are equal')
+// }
+// else
+// {
+//     console.log('???????')
+// }
+//
+//
+// let a = 'true'
+// console.log(!!a)
+// if (a !== true)
+// {
+//     let def = 'bad'
+//     a = def
+//     console.log(def)
+// }
+// else
+// {
+//     console.log(a)
+// }
+//
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// for (let i = 0; i < coursesAndDurationArray.length; i++)
+// {
+//     if (coursesAndDurationArray[i].monthDuration > 5)
+//     {
+//         console.log(coursesAndDurationArray[i].title, 'Super')
+//     }
+// }
+
+
+
